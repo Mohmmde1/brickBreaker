8efae7e2798f69af591be382adadcadbff4f1735
Mohammed Fuad <mohammedfuadalnashriy@gmail.com>
2022-04-30 04:55:22 +0800

    initalize the gameObjects design

---
+++ src/main/java/game/components/Brick.java
@@ -0,0 +1,17 @@
+package game.components;
+
+import game.components.GameObject;
+
+public class Brick extends GameObject{
+    // hittalbe type var
+    // hittalbe type;
+    int strength;
+
+    Brick() {
+
+    }
+
+    void hit() {
+    // TODO Auto-generated method stub
+    }
+}
---
+++ src/main/java/game/components/GameObject.java
@@ -0,0 +1,26 @@
+package game.components;
+
+import java.awt.Point;
+
+import java.awt.Graphics;
+
+import java.awt.Rectangle;
+
+public class GameObject extends Rectangle {
+    private Point point;
+
+    GameObject(){
+
+    }
+
+    public void paint(Graphics g) {
+         // TODO Auto-generated method stub
+    }
+
+    public void intersect(GameObject obj) {
+         // TODO Auto-generated method stub
+    }
+
+    
+
+}
